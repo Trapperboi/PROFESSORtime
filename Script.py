37962466a6b1dd4df5ed032e5f385d913ba7f361
trapperboy <92212355+Trapperboi@users.noreply.github.com>
2023-01-17 09:57:57 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -30,7 +30,7 @@ class script(object):
 
 <code>/batch https://t.me/mkn_bots_updates https://t.me/mkn_bots_updates</code>
 
-𝙲𝚁𝙴𝙳𝙸𝚃𝚂 ›› <a href=https://t.me/mkn_bots_updates><b>𝙼𝙺𝙽 𝙱𝙾𝚃𝚉</b></a>"""
+𝙲𝚁𝙴𝙳𝙸𝚃𝚂 ›› <a href=https://t.me/MsBotTg1><b>𝙼MsBotTg</b></a>"""
     
     MANUELFILTER_TXT = """Help: <b>Filters</b>
 
